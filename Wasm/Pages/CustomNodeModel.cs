494f1326ce8b728d34c8591bc454ff6d1d68abaa
Jan <jan.hadascok@cz.abb.com>
2024-09-11 10:50:59 +0200

    Add project files.

---
+++ Wasm/Pages/CustomNodeModel.cs
@@ -0,0 +1,15 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace Wasm.Pages
+{
+    public class CustomNodeModel : NodeModel
+    {
+        public CustomNodeModel(Point position, bool showsTitle) : base(position)
+        {
+            ShowsTitle = showsTitle;
+        }
+
+        public bool ShowsTitle { get; set; }
+    }
+}
